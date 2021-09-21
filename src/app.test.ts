264f1364526af90cf9d4de1e9efae9f032db4032
Daniel Malagurti <danielmalagurti@gmail.com>
2021-09-20 22:40:38 -0400

    test: create all tests

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,6 +1,14 @@
-import { invoiceCpf } from './app';
+import { invoiceCpf, totalOrderedItens, discountToken  } from './app';
 
 test("Should validade cpf", function () {
     expect(invoiceCpf).toBe("03816446183");
+});
+
+test("Should totalOrderedItens be more than 3", function () {
+    expect(totalOrderedItens).toBeGreaterThanOrEqual(3);
+});
+
+test("Should discountToke be true", function () {
+    expect(discountToken).toBeTruthy;
 });
 
