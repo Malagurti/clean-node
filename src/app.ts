89b07af056f57e9c49e1342abbf70ad177e04488
Daniel Malagurti <danielmalagurti@gmail.com>
2021-09-20 22:17:58 -0400

    feat: create CreateOrder

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+export const totalOrderedItens = 3
+export const invoiceCpf = "03816446183"
+export const total = 200.3
+export const discount = 20
+export const discountToken = true
+
+function CreateOrder (totalOrderedItens:number, invoiceCpf:String, total:number, discount:number){
+    if (discountToken){
+        const totalOrderPrice = (total-(((total)) * discount/100))
+        const order = {
+            "finalPrice": totalOrderPrice,
+            "invoiceCpf": invoiceCpf,
+            "totalOrderedItens": totalOrderedItens,
+        }
+        const savedOrder = order
+        console.log(savedOrder)
+    }
+    else{
+        console.log("Erro no envio da Ordem")
+    }
+}
+CreateOrder(totalOrderedItens, invoiceCpf, total, discount)
